e32def1697e74ddeed74a749d315482bc836dc1a
松澤春希 <haruki@matsuzawaharunozominoMacBook-Air.local>
2026-05-10 11:56:16 +0900

    fix render port

--- server.js
+++ server.js
@@ -73,6 +73,8 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(3000, () => {
-    console.log("サーバー起動: http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`サーバー起動: ${PORT}`);
 });
